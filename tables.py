1bfd1c886ba8b0aa02d7c0db2ab207f0d5affac8
mahnoor <mahnoor.malik45@gmail.com>
2021-11-17 16:36:35 +0500

    created db connection file

--- tables.py
+++ tables.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.sql import text
+from pathlib import Path
 
-engine = create_engine("postgresql:///FMS", echo=True)
+engine = create_engine(Path("db_connection").read_text(), echo=True)
 with engine.connect() as con:
     con.execute(text("""
     create table accounts(
