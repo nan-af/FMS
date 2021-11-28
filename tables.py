33ffb785135531edce2851820cd18c07d5c4b8aa
mahnoor <mahnoor@LAPTOP-7V5K9OAN.localdomain>
2021-11-28 20:59:19 +0500

    updating all tables

--- tables.py
+++ tables.py
@@ -103,3 +103,94 @@ with engine.connect() as con:
         references employee(employee_id)
         on update cascade
     )"""))
+
+    # creates STOCK table
+    con.execute(text("""
+    create table stock(
+        stock_id serial primary key,
+        quantity int,
+        location varchar(30),
+        total_weight numeric,
+        received_date date,
+        use_date date,
+        type 
+    )"""))
+
+    # creates PURCHASED FROM table
+    con.execute(text("""
+    create table purchased(
+        stock_id serial, primary key,
+        vendor_id serial,
+        transaction_id serial,
+        PRIMARY KEY(stock_id, vendor_id, transaction_id)
+
+    )"""))
+
+    # creates ALLOWANCE table
+    con.execute(text("""
+    create table allowance(
+        type varchar(30)
+
+    )"""))
+
+    # creates UTILISED table
+    con.execute(text("""
+    create table utilised(
+        type varchar(30),
+        employee_id serial,
+        transaction_id serial,
+        PRIMARY KEY(type, employee_id, transaction_id)
+
+    )"""))
+
+    # creates E_AT table
+    con.execute(text("""
+    create table e_at(
+        date_today date primary key,
+        employee_id serial,
+        transaction_id serial,
+        PRIMARY KEY(date_today, employee_id, transaction_id)
+
+    )"""))
+
+    # creates ADVANCE table
+    con.execute(text("""
+    create table advance(
+
+    foreign key(transaction_id) 
+    references transactions(transaction_id)
+    on update cascade,
+
+    foreign key(employee_id) 
+    references employee(employee_id)
+    on update cascade,
+
+    date_today date,
+    PRIMARY KEY(transaction_id)
+        
+
+    )"""))
+
+
+
+    # creates ORDER table
+    con.execute(text("""
+    create table order(
+        order_id serial primary key,
+        quantity int,
+        item_name varchar(15),
+        due date date,
+
+        foreign key(transaction_id) 
+        references transactions(transaction_id)
+        on update cascade,
+
+        foreign key(customer_id) 
+        references customer(customer_id)
+        on update cascade,
+
+        foreign key(stock_id) 
+        references stock(stock_id)
+        on update cascade
+        
+    )"""))
